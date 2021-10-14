7befb86ec1a16856cb2b715f96c706e21a6e7937
zhuaowei <z2845191711@163.com>
2021-10-14 15:59:03 +0800

    添加主页管理功能

--- src/main/java/cn/huanzi/qch/baseadmin/ccb/homepage/controller/HomePageController.java
+++ src/main/java/cn/huanzi/qch/baseadmin/ccb/homepage/controller/HomePageController.java
@@ -1,10 +1,14 @@
 package cn.huanzi.qch.baseadmin.ccb.homepage.controller;
 
+import cn.huanzi.qch.baseadmin.ccb.homepage.pojo.HomePage;
 import cn.huanzi.qch.baseadmin.ccb.homepage.service.HomePageService;
+import cn.huanzi.qch.baseadmin.common.pojo.Result;
 import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -42,5 +46,60 @@ public class HomePageController {
         return mav;
     }
 
+    @GetMapping("page")
+    public Result page(Integer page, Integer limit) {
+        if (page == null || page < 1) {
+            page = 1;
+        }
+        if (limit == null || limit < 1) {
+            limit = 10;
+        }
 
+        Page<HomePage> homePages = homePageService.pagination(page - 1, limit);
+        return Result.of(homePages);
+    }
+
+    @GetMapping("get")
+    public Result get(Integer id) {
+        if (id == null) {
+            return Result.of(id, false, 400, "请输入id");
+        }
+        HomePage byId = homePageService.getById(id);
+        if (byId == null) {
+            return Result.of(null, false, 400, "未查找到数据");
+        } else {
+            return Result.of(byId);
+        }
+    }
+
+    @PostMapping("create")
+    public Result create(HomePage homePage) {
+        try {
+            HomePage save = homePageService.create(homePage);
+            return Result.of(save);
+        } catch (Exception e) {
+            return Result.of(null, false, 400, "保存失败");
+        }
+    }
+
+    @PostMapping("update")
+    public Result update(HomePage homePage) {
+        try {
+            HomePage save = homePageService.update(homePage);
+            return Result.of(save);
+        } catch (Exception e) {
+            return Result.of(null, false, 400, "保存失败");
+        }
+    }
+
+    @PostMapping("delete")
+    public Result delete(Integer id) {
+        try {
+            homePageService.deleteById(id);
+            return Result.of(id);
+        } catch (Exception e) {
+            return Result.of(null, false, 400, "删除失败");
+        }
+
+    }
 }
---
+++ src/main/resources/static/ccb/homepage/js/homepage.js
@@ -0,0 +1,112 @@
+let tableIns;
+let contextPath = window.location.protocol + "://" + window.location.host;
+layui.use(['table', 'form', 'upload', 'layer', 'element'], function(){
+    var table = layui.table
+        , layer = layui.layer;
+
+    tableIns = table.render({
+        elem: '#homepageTable'
+        , height: 'full-200'
+        , cellMinWidth: 80
+        , page: true
+        ,url:'/ccb/homepage/page'
+        , method: 'GET'
+        //请求前参数处理
+        , request: {
+            pageName: 'page' //页码的参数名称，默认：page
+            , limitName: 'limit' //每页数据量的参数名，默认：limit
+        }
+        , response: {
+            statusName: 'flag' //规定数据状态的字段名称，默认：code
+            , statusCode: true //规定成功的状态码，默认：0
+            , msgName: 'msg' //规定状态信息的字段名称，默认：msg
+            , countName: 'records' //规定数据总数的字段名称，默认：count
+            , dataName: 'rows' //规定数据列表的字段名称，默认：data
+        }
+        //响应后数据处理
+        , parseData: function (res) { //res 即为原始返回的数据
+            var data = res.data;
+            return {
+                "flag": res.flag, //解析接口状态
+                "msg": res.msg, //解析提示文本
+                "records": data.totalElements, //解析数据长度
+                "rows": data.content //解析数据列表
+            };
+        }
+        , toolbar: '#homepageTableToolbarDemo'
+        , title: '主页列表'
+        ,cols: [
+            [
+                { field:'id', width:80, title: 'ID', sort: true }
+                , { field:'activity', width:300, title: '活动介绍', sort: true }
+                , { fixed: 'right', width:300, align:'center', toolbar: '#homepageBarDemo' }
+            ]
+        ]
+    });
+
+    //头工具栏事件
+    table.on('toolbar(homepage)', function(obj){
+        switch(obj.event){
+            case 'createHomepage':
+                layer.open({
+                    type: 2,
+                    title: '新增',          // 弹窗标题
+                    shadeClose: true,           //弹出框之外的地方是否可以点击
+                    offset: 'auto',
+                    area: ['60%', '80%'],
+                    content: '/ccb/homepage/homepageForm',
+                    end: function () {
+                        tableIns.reload();
+                    }
+                });
+                break;
+
+            //自定义头工具栏右侧图标 - 提示
+            case 'LAYTABLE_TIPS':
+                layer.alert('这是工具栏右侧自定义的一个图标按钮');
+                break;
+        };
+    });
+
+    // 监听右侧工具条
+    table.on('tool(homepage)', function(obj){
+        let data = obj.data;
+        if(obj.event === 'del'){
+            /* 删除操作 */
+            layer.confirm('真的删除行么', function(index){
+                $.post("/ccb/homepage/delete", {
+                    "id": data.id
+                }, function (data) {
+                    console.log(data.msg);
+                }, "json");
+                layer.close(index);
+                tableIns.reload();
+            });
+        } else if(obj.event === 'edit'){
+            /* 编辑操作 */
+            layer.open({
+                type: 2,
+                title: '编辑',          // 弹窗标题
+                shadeClose: true,           //弹出框之外的地方是否可以点击
+                offset: 'auto',
+                area: ['60%', '80%'],
+                content: '/ccb/homepage/homepageForm?id=' + data.id,
+                end: function () {
+                    tableIns.reload();
+                }
+            });
+        } else if (obj.event === 'video') {
+            // 查看视频
+            var videoElem =
+                '<video width="100%" height="100%" controls autobuffer autoplay="autoplay" loop="loop">' +
+                '<source src="' + data.videoUrl + '" type="video/mp4"/>' +
+                '</video>'
+            layer.open({
+                type: 1,
+                area: ['960px', '600px'],
+                title: '播放视频',
+                content: videoElem,
+            });
+        }
+    });
+});
